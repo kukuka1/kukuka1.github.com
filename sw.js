90a4282edfaf3df84dc3d1a551442fa637557219
kukuka1 <38097791+kukuka1@users.noreply.github.com>
2025-07-11 14:20:35 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -30,9 +30,8 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
