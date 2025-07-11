0430db8bf767418c1381f3ccfcfa8fba577a2bb7
kukuka1 <38097791+kukuka1@users.noreply.github.com>
2025-07-11 12:34:31 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vegetable Lists</title>
+  <link rel="stylesheet" href="styles.css">
+  <link rel="manifest" href="/manifest.json">
+</head>
+<body>
+  <div class="grid-container"></div>
+  <script src="script.js"></script>
+</body>
+</html>
